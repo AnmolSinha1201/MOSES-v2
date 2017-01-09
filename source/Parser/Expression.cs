a81762e88ba36252b27eb3293d820fc81c76a427
Anmol Sinha <anmolsinha1201@outlook.com>
2017-01-09 12:03:10 +0530

    changed standard parser return type to string

--- source/Parser/Expression.cs
+++ source/Parser/Expression.cs
@@ -2,11 +2,11 @@ namespace MOSESParser
 {
     partial class Parser
     {
-        object Expression(string code, ref int origin)
+        string Expression(string code, ref int origin)
         {
             return constantExpression(code, ref origin);
         }
-        object constantExpression(string code, ref int origin)
+        string constantExpression(string code, ref int origin)
         {
             return STRING(code, ref origin) ?? NUMBER(code, ref origin);
         }
